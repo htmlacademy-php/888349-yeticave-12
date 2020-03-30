6239f9c2c7cc79d2fba7924f2393e237406c8541
shevchukroma <shevchuk.roma2@gmail.com>
2020-03-30 19:11:24 +0300

    Первое задание второго модуля

--- index.php
+++ index.php
@@ -2,6 +2,45 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Роман'; // укажите здесь ваше имя
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$adverts = [
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price'   => '10999',
+        'image_url'   => 'img/lot-1.jpg',
+    ],
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price'   => '159999',
+        'image_url'   => 'img/lot-2.jpg',
+    ],
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price'   => '8000',
+        'image_url'   => 'img/lot-3.jpg',
+    ],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price'   => '10999',
+        'image_url'   => 'img/lot-4.jpg',
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price'   => '7500',
+        'image_url'   => 'img/lot-5.jpg',
+    ],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price'   => '5400',
+        'image_url'   => 'img/lot-6.jpg',
+    ],
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -55,9 +94,11 @@ $user_name = 'Роман'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+            <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $category?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
