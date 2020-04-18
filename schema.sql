88c7d76320aee10ef958ae1ba608186fda9499bc
shevchukroma <shevchuk.roma2@gmail.com>
2020-04-18 20:19:59 +0300

    fix

--- schema.sql
+++ schema.sql
@@ -5,26 +5,30 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE category (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(128) NOT NULL,
-                            symbol_code VARCHAR(128)
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(128) NOT NULL,
+                    symbol_code VARCHAR(128)
 );
 
 CREATE TABLE lot (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         created_date DATETIME,
-                         name VARCHAR(128) NOT NULL,
-                         description VARCHAR(2560),
-                         img_link VARCHAR(255),
-                         price INT NOT NULL,
-                         expiration_date DATETIME,
-                         step_bets INT
+                     id INT AUTO_INCREMENT PRIMARY KEY,
+                     created_date DATETIME,
+                     name VARCHAR(128) NOT NULL,
+                     description VARCHAR(2560),
+                     img_link VARCHAR(255),
+                     price INT NOT NULL,
+                     expiration_date DATETIME,
+                     step_bets INT,
+                     creatorId INT,
+                     winnerId INT
 );
 
 CREATE TABLE bet (
                      id INT AUTO_INCREMENT PRIMARY KEY,
                      date DATETIME,
-                     price INT NOT NULL
+                     price INT NOT NULL,
+                     userId INT,
+                     lotId INT
 );
 
 CREATE TABLE user (
@@ -33,5 +37,12 @@ CREATE TABLE user (
                      email VARCHAR(128),
                      name VARCHAR(128) NOT NULL,
                      password VARCHAR(64),
-                     contact VARCHAR(2560)
+                     contact VARCHAR(2560),
+                     createdLotId INT,
+                     createdBetId INT
 );
+
+# DROP TABLE category;
+# DROP TABLE lot;
+# DROP TABLE bet;
+# DROP TABLE user;
